fea696e054efb4f48649f72e13cf923ffbf7f216
ninkadp <ninadepalma@gmail.com>
2021-11-18 10:08:40 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2020
+
+Learning python through Advent of Code puzzles (2020)
+
+https://adventofcode.com/
+https://adventofcode.com/2020
---
+++ day1/day1.py
@@ -0,0 +1,21 @@
+def part_one(expenses):
+    for expense in expenses:
+        for expense2 in expenses:
+            if expense + expense2 == 2020:
+                return expense * expense2
+
+def part_two(expenses):
+    for expense in expenses:
+        for expense2 in expenses:
+            for expense3 in expenses:
+                if expense + expense2 + expense3 == 2020:
+                    return expense * expense2 * expense3
+
+expenses = []
+
+with open(r'C:\Users\ndepalma\Desktop\advent_2020\day1\input.txt') as f:
+    for expense in f.readlines():
+        expenses.append(int(expense.strip()))
+
+print(f'The answer to part one is {part_one(expenses)}')
+print(f'The answer to part two is {part_two(expenses)}')
