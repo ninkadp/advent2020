84865776119d73acf8c9b4aeada9e6499959a618
ninkadp <ninadepalma@gmail.com>
2021-11-18 10:13:01 -0500

    update day1

--- day1/day1.py
+++ day1/day1.py
@@ -11,11 +11,12 @@ def part_two(expenses):
                 if expense + expense2 + expense3 == 2020:
                     return expense * expense2 * expense3
 
-expenses = []
+if __name__ == 'main':
+    expenses = []
 
-with open(r'C:\Users\ndepalma\Desktop\advent_2020\day1\input.txt') as f:
-    for expense in f.readlines():
-        expenses.append(int(expense.strip()))
+    with open(r'C:\Users\ndepalma\Desktop\advent_2020\day1\input.txt') as f:
+        for expense in f.readlines():
+            expenses.append(int(expense.strip()))
 
-print(f'The answer to part one is {part_one(expenses)}')
-print(f'The answer to part two is {part_two(expenses)}')
+    print(f'The answer to part one is {part_one(expenses)}')
+    print(f'The answer to part two is {part_two(expenses)}')
